b8deca1fa2ca8c3455143edd4a11f4771bf222fb
Rafael Marçalo <im.suggon@gmail.com>
2026-04-25 13:09:20 +0100

    UI Refactor

---
+++ ui/lower_buttons_frame.py
@@ -0,0 +1,58 @@
+import os
+import tkinter as tk
+
+from services.ip_utils import collect_ips
+from services.firewall import generate_firewall_rules, apply_rules, remove_rules
+
+blockServerString = "Block Servers"
+unblockServerString = "Unblock Servers"
+cleanupRulesString = "Cleanup Rules"
+datasetErrorString = "No dataset loaded"
+ipErrorString = "No IPs found"
+
+def apply_rules_ui(tree, executable, store):
+    data = store["data"]
+    exe_path = executable.path.get()
+
+    if not data:
+        print(datasetErrorString)
+        return
+
+    selected = tree.get_selected()
+    ips = collect_ips(data, selected)
+
+    if not ips:
+        print(ipErrorString)
+        return
+
+    rules = generate_firewall_rules(exe_path, ips)
+
+    apply_rules(rules)
+
+def toggle_rules(tree, executable, store, state, label_var):
+    if state["value"]:
+        remove_rules()
+        label_var.set(blockServerString)
+        state["value"] = False
+    else:
+        apply_rules_ui(tree, executable, store)
+        label_var.set(unblockServerString)
+        state["value"] = True
+
+class LowerButtonsFrame(tk.Frame):
+    def __init__(self, parent, tree, executable, data_store, rules_active):
+        super().__init__(parent)
+
+        self.button_text = tk.StringVar(value=blockServerString)
+
+        tk.Button(
+            master=self,
+            textvariable=self.button_text,
+            command=lambda: toggle_rules(tree, executable, data_store, rules_active, self.button_text)
+        ).pack(side="left", padx=5)
+
+        tk.Button(
+            master=self,
+            text=cleanupRulesString,
+            command=remove_rules
+        ).pack(side="left", padx=5)
--- ui/ui.py
+++ ui/ui.py
@@ -3,9 +3,7 @@ import tkinter as tk
 from .dataset_frame import DatasetFrame
 from .executable_frame import ExecutableFrame
 from .tree_view import TreeView
-
-from services.ip_utils import collect_ips
-from services.firewall import generate_firewall_rules, apply_rules, remove_rules
+from .lower_buttons_frame import LowerButtonsFrame
 
 SIZE = 512
 
@@ -23,51 +21,15 @@ def startUI():
         on_load=lambda data: load_dataset(tree, data_store, data)
     )
     executable = ExecutableFrame(root)
+    lowerButtons = LowerButtonsFrame(root, tree, executable, data_store, rules_active)
 
     dataset.pack(anchor="w", pady=5)
     executable.pack(anchor="w", pady=5)
     tree.pack(fill="both", expand=True, pady=5)
-
-    button_text = tk.StringVar(value="Block Servers")
-
-    tk.Button(
-        master=root,
-        textvariable=button_text,
-        command=lambda: toggle_rules(tree, executable, data_store, rules_active, button_text)
-    ).pack(anchor="w", padx=5, pady=5)
+    lowerButtons.pack(anchor="w", pady=5)
 
     root.mainloop()
 
 def load_dataset(tree, store, data):
     store["data"] = data
     tree.load_data(data)
-
-def toggle_rules(tree, executable, store, state, label_var):
-    if state["value"]:
-        remove_rules()
-        label_var.set("Block Servers")
-        state["value"] = False
-    else:
-        apply_rules_ui(tree, executable, store)
-        label_var.set("Unblock Servers")
-        state["value"] = True
-
-
-def apply_rules_ui(tree, executable, store):
-    data = store["data"]
-    exe_path = executable.path.get()
-
-    if not data:
-        print("No dataset loaded")
-        return
-
-    selected = tree.get_selected()
-    ips = collect_ips(data, selected)
-
-    if not ips:
-        print("No IPs found")
-        return
-
-    rules = generate_firewall_rules(exe_path, ips)
-
-    apply_rules(rules)
