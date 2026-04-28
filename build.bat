95daea75272855a847cacbd936d24bb5d948af83
Rafael Marçalo <im.suggon@gmail.com>
2026-04-28 19:30:04 +0100

    Updated build script

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 
-pyinstaller -F -n ssb -w --uac-admin -i assets\app.ico --add-data "assets/app.ico;assets" ssb.py
+python -m PyInstaller -F -n ssb -w --uac-admin -i assets\app.ico --add-data "assets/app.ico;assets" ssb.py
